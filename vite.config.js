fd386cc2118159e100e5027af07010b52dd95d1e
Asror Akbarkhodjaev <asror9800@mail.ru>
2025-09-12 16:56:55 +0500

    small changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sochma-frontend/',
+  base: '/sochma_frontend/',
   plugins: [
     vue(),
   ],
